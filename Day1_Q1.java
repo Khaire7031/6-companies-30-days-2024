6e2b17653ade16df7952182b6a238d209f70acc5
Pranav Khaire <pranavkhaire28@gmail.com>
2023-12-31 12:00:58 +0530

    First commit

---
+++ Day1_Q1.java
@@ -0,0 +1,21 @@
+
+/*
+    We have two arrays arr1 and arr2 which are initially empty. You need to add positive integers to them such that they satisfy all the following conditions:
+
+    arr1 contains uniqueCnt1 distinct positive integers, each of which is not divisible by divisor1.
+    arr2 contains uniqueCnt2 distinct positive integers, each of which is not divisible by divisor2.
+    No integer is present in both arr1 and arr2.
+    Given divisor1, divisor2, uniqueCnt1, and uniqueCnt2, return the minimum possible maximum integer that can be present in either array.
+*/
+
+
+import java.util.*;
+class Day1_Q1{
+    public static int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("        Question 1: ");
+    }
+}
